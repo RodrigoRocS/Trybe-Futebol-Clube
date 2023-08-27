df4bd9dec23802bd795487059614367be1298217
root <root@DESKTOP-DFB6OFB>
2023-08-27 15:30:59 -0300

    feat: team interface migration and model

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,34 @@
+import {
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from 'sequelize';
+import db from '.';
+
+class Team extends Model
+  <InferAttributes<Team>,
+  InferCreationAttributes<Team>> {
+  declare id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Team;
